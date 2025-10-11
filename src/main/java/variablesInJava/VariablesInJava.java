f096f6e632fc5b28c3a36aa1c531291db19269f2
shivam <shivam12397@gmail.com>
2025-10-11 20:36:21 +0530

    Working with variables in Java
    - Declaration and initialization of variables in Java.

---
+++ src/main/java/variablesInJava/VariablesInJava.java
@@ -0,0 +1,12 @@
+package variablesInJava;
+
+public class VariablesInJava {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 9;
+        //int a =3, b = 9, c = a*b;
+        // All variables can be declared in one single line as well.
+        int c = a * b;
+        System.out.println("c = "+ c);
+    }
+}
