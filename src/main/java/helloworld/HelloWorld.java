ab47eaf86deae127329a7c53a916ce0fa2f579fd
shivam <shivam12397@gmail.com>
2025-10-11 15:39:22 +0530

    HelloWorld program
    - Completed the HelloWorld Program
    - Tried something with the args passed in the main method.

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -2,6 +2,14 @@ package helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("Hello, world!");
 
+        //changing the args in the main method using the run configurations
+        //System.out.println("Running and checking the arguments that I changed in the run configurations: " + args.length);
+
+        //This is the enhanced for loop or the for-each loop code shown below
+//        for(String element : args) {
+//            System.out.println("Argument: " + element);
+//        }
     }
 }
