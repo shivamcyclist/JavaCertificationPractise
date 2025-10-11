1760fba12cad44f54bb427c4bb2a555607cd9c42
shivam <shivam12397@gmail.com>
2025-10-11 15:57:16 +0530

    Basic Output
    - Practising basic output in Java.

---
+++ src/main/java/BasicOutput/BasicOutputInJava.java
@@ -0,0 +1,16 @@
+package BasicOutput;
+
+public class BasicOutputInJava {
+    public static void main(String[] args) {
+        System.out.println("Java Programming is fun");
+
+        System.out.print("Java ");
+        System.out.print("Programming ");
+        System.out.print("is ");
+        System.out.print("fun"+ "\n");
+
+        int a = 27;
+        System.out.printf("%d",a);
+
+    }
+}
