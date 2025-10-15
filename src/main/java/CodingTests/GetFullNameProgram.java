f01d38b6d42d5ad339312cd2b426e7835b7b5c4b
shivam <shivam12397@gmail.com>
2025-10-15 19:58:55 +0530

    First Test Program
    - A program to get full name from user and display the input name.

---
+++ src/main/java/CodingTests/GetFullNameProgram.java
@@ -0,0 +1,22 @@
+package CodingTests;
+
+import java.util.Scanner;
+
+public class GetFullNameProgram {
+
+    static String firstName, lastName;
+
+    public static void main(String[] args) {
+        System.out.println(getFullName());
+    }
+
+    public static String getFullName() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first name");
+        firstName = input.next();
+        System.out.println("Enter last name");
+        lastName = input.next();
+        String fullName = firstName + " " + lastName;
+        return fullName;
+    }
+}
